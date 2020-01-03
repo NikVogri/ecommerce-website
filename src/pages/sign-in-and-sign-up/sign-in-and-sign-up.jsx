bb5b25d075c367046b310d9dece867710d143b70
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-01-03 22:12:04 +0100

    added firebase  authentication

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 const SignInAndSignUp = () => {
   return (
-    <div>
+    <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   );
 }
