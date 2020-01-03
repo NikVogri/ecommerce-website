357e84d82da04f99b73282d286ea86820ae14c60
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-01-03 22:17:23 +0100

    finished firebase authentication

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 
 export default class SignIn extends Component {
   constructor(props) {
@@ -13,9 +13,19 @@ export default class SignIn extends Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
     this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: '',
+        password: ''
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   handleChange = event => {
