2b0084fdb9165ac07c01a3a61e66672e355675ff
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-01-05 00:08:28 +0100

    added cart dropdown

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,9 +5,12 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
+
 import { connect } from 'react-redux';
 
-const header = ({ currentUser }) => {
+const header = ({ currentUser, hidden }) => {
   return (
     <div className='header'>
       <Link to='/' className='logo-container'>
@@ -19,13 +22,19 @@ const header = ({ currentUser }) => {
         {
           currentUser ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> : <Link className='option' to='/signin'>SIGN IN</Link>
         }
+        <CartIcon />
       </div>
+      {
+        hidden ? null : <CartDropdown />
+      }
     </div>
   );
 }
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
+
 });
 
 export default connect(mapStateToProps)(header);
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,4 @@
+import { CartActionTypes } from './cartActionTypes';
+
+
+export const toggleCartHidden = () => ({ type: CartActionTypes.TOGGLE_CART_HIDDEN })
