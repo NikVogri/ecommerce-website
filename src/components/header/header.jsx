d2fd44513e7c0aa6bd7bcf78b558ae78e7099f30
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-01-08 21:17:22 +0100

    added styled header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './header.styles.scss';
 
-import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { createStructuredSelector } from 'reselect';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
 import { connect } from 'react-redux';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles';
 
 import CartIcon from '../cart-icon/cart-icon';
 import CartDropdown from '../cart-dropdown/cart-dropdown';
@@ -16,22 +16,22 @@ import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 const header = ({ currentUser, hidden }) => {
   return (
-    <div className='header'>
-      <Link to='/' className='logo-container'>
+    <HeaderContainer className='header'>
+      <LogoContainer to='/' className='logo-container'>
         <Logo className='logo' />
-      </Link>
-      <div className="options">
-        <Link to='/shop' className='option'>SHOP</Link>
-        <Link to='/shop' className='option'>CONTACT</Link>
+      </LogoContainer>
+      <OptionsContainer className="options">
+        <OptionLink to='/shop' className='option'>SHOP</OptionLink>
+        <OptionLink to='/shop' className='option'>CONTACT</OptionLink>
         {
-          currentUser ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> : <Link className='option' to='/signin'>SIGN IN</Link>
+          currentUser ? <OptionDiv className='option' onClick={() => auth.signOut()}>SIGN OUT</OptionDiv> : <OptionLink className='option' to='/signin'>SIGN IN</OptionLink>
         }
         <CartIcon />
-      </div>
+      </OptionsContainer>
       {
         hidden ? null : <CartDropdown />
       }
-    </div>
+    </HeaderContainer>
   );
 }
 
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import './homepage.styles.scss';
-
+import HomePageContainer from './homepage.styles';
 import Directory from '../../components/directory/directory';
 
 const Homepage = () => {
   return (
-    <div className='homepage'>
+    <HomePageContainer className='homepage'>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 }
 
